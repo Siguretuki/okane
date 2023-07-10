8d2548e33608a5dd6e1d4422ff7e1e4b5d82b126
Siguretuki <101462925+Siguretuki@users.noreply.github.com>
2023-07-10 05:26:40 +0000

    toppage作成

---
+++ components/Toppage.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-3">
+            <h1 class="text-center">各種入力</h1>
+                <div class="mx-auto">
+                    <ul class="list-unstyled small my-3">
+                        <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">複合仕訳入力</button>
+                        </li>
+                        <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">元帳入力</button>
+                        </li>
+                        <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">通帳入力</button>
+                        </li>
+                        <li class="my-3"> </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-9">
+            <h1 class="text-center">各種出力</h1>
+                <div class="row">
+                    <div class="col-4">
+                        <ul class="list-unstyled small my-3">
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">仕訳帳</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">合計残高試算表</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">総勘定元帳</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">個人出納帳</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul class="list-unstyled small my-3">
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">出納帳</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">補助科目合計残高表</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">補助元帳</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">経費精算申請</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul class="list-unstyled small my-3">
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">仕訳検索</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">消費税課税区分別集計表</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">月次比較財務諸表</button>
+                            </li>
+                            <li class="my-3">
+                            <button type="button" class="btn btn-outline-primary mx-auto">経費精算承認</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
